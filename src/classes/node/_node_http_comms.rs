6d90ab26d11f0f2e2bb706090919263d1c0e7e32
Muhamad Aji Wibisono <muhamadajiw71@gmail.com>
2025-04-17 20:31:27 +0700

    feat: http endpoints

--- src/classes/node/_node_http_comms.rs
+++ src/classes/node/_node_http_comms.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use actix_web::{web, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
-use tokio::sync::Mutex;
+use tokio::{sync::Mutex, time::Instant};
 
 use crate::base_libs::_operation::{BinKV, Operation, OperationType};
 
@@ -24,6 +24,12 @@ pub struct DeleteBody {
     key: String,
 }
 
+#[derive(Serialize)]
+struct BaseResponse {
+    key: String,
+    response: String,
+}
+
 impl Node {
     pub async fn http_healthcheck(node_data: web::Data<Arc<Mutex<Node>>>) -> impl Responder {
         let node = node_data.lock().await;
@@ -35,37 +41,96 @@ impl Node {
         node_data: web::Data<Arc<Mutex<Node>>>,
         query: web::Query<GetQuery>,
     ) -> impl Responder {
+        println!("[REQUEST] get request received for key: {}", query.key);
         let node = node_data.lock().await;
-        HttpResponse::Ok().body(format!("GET: key = {}", query.key))
+        let operation = Operation {
+            op_type: OperationType::GET,
+            kv: BinKV {
+                key: query.key.clone(),
+                value: vec![],
+            },
+        };
+        let result = node
+            .store
+            .process_request(&operation, &node)
+            .await
+            .unwrap_or_default();
+
+        let response = BaseResponse {
+            key: query.key.clone(),
+            response: result,
+        };
+
+        let mut last_heartbeat_mut = node.last_heartbeat.write().await;
+        *last_heartbeat_mut = Instant::now();
+
+        HttpResponse::Ok().json(response)
     }
 
     pub async fn http_post(
         node_data: web::Data<Arc<Mutex<Node>>>,
         body: web::Json<PostBody>,
     ) -> impl Responder {
+        println!(
+            "[REQUEST] post request received for key: {}, value {}",
+            body.key, body.value
+        );
         let mut node = node_data.lock().await;
-        let op = Operation {
+        node.request_id += 1;
+
+        let operation = Operation {
             op_type: OperationType::SET,
             kv: BinKV {
                 key: body.key.clone(),
                 value: body.value.clone().into_bytes(),
             },
         };
-        HttpResponse::Ok().body(format!("POST: key = {}, value = {}", body.key, body.value))
+        let result = node
+            .store
+            .process_request(&operation, &node)
+            .await
+            .unwrap_or_default();
+
+        let response = BaseResponse {
+            key: body.key.clone(),
+            response: result,
+        };
+
+        let mut last_heartbeat_mut = node.last_heartbeat.write().await;
+        *last_heartbeat_mut = Instant::now();
+
+        HttpResponse::Ok().json(response)
     }
 
     pub async fn http_delete(
         node_data: web::Data<Arc<Mutex<Node>>>,
         body: web::Json<DeleteBody>,
     ) -> impl Responder {
+        println!("[REQUEST] delete request received for key: {}", body.key);
         let mut node = node_data.lock().await;
-        let op = Operation {
+        node.request_id += 1;
+
+        let operation = Operation {
             op_type: OperationType::DELETE,
             kv: BinKV {
                 key: body.key.clone(),
                 value: vec![],
             },
         };
-        HttpResponse::Ok().body(format!("Delete: key = {}", body.key))
+        let result = node
+            .store
+            .process_request(&operation, &node)
+            .await
+            .unwrap_or_default();
+
+        let response = BaseResponse {
+            key: body.key.clone(),
+            response: result,
+        };
+
+        let mut last_heartbeat_mut = node.last_heartbeat.write().await;
+        *last_heartbeat_mut = Instant::now();
+
+        HttpResponse::Ok().json(response)
     }
 }
